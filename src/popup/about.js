060ab61ca2de2bc133751b3c08c7bd323aaca54c
冯思程 <fscdyx888@163.com>
2024-09-26 20:23:31 +0800

    feat(tab): add other tab(function page)

---
+++ src/popup/about.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 这里可以添加该分区的特定功能
+    console.log("Another Tab functionality loaded.");
+});
---
+++ src/popup/tabs.js
@@ -0,0 +1,13 @@
+// tabs.js
+function showTab(tabName) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+      tab.style.display = 'none'; 
+    });
+    document.getElementById(tabName).style.display = 'block'; // 显示当前分区
+}
+  
+document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('button[onclick="showTab(\'memo\')"]').addEventListener('click', () => showTab('memo'));
+document.querySelector('button[onclick="showTab(\'about\')"]').addEventListener('click', () => showTab('about'));
+});
